397c6b58a6638562c584c837fd9ab43f7c54bd43
Easy Snezhok <morshemor6@gmail.com>
2026-09-25 13:37:46 +0300

    security

--- adidas-store/src/firebase.js
+++ adidas-store/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-2U5KiTrnOefWfCYJAx3XgIqJqgxIuzc",
-  authDomain: "adidas-store-318bb.firebaseapp.com",
-  projectId: "adidas-store-318bb",
-  storageBucket: "adidas-store-318bb.firebasestorage.app",
-  messagingSenderId: "76480732007",
-  appId: "1:76480732007:web:bf61e2c4050b86852a2dc2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
